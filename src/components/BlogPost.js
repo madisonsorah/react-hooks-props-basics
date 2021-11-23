7529434095087a83ab00089feb80db13b1eb3b03
madison sorah <madisonsorah@gmail.com>
2021-11-23 14:39:09 -0500

    Completed assignment

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,12 +5,22 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." 
+      isPublished={true}
+      minutesToRead={1}/>
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
     </div>
   );
 }
+
+// function BlogPost() {
+//   return (
+//     <div>
+//       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+//     </div>
+//   );
+// }
 
 export default BlogPost;
